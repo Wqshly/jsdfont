21d730a688fa964559c0b6f69a8b1dd7c23a6aaa
91074 <910749592@qq.com>
2020-09-27 20:32:16 +0800

    修改了全局设置，增加了请求拦截和响应拦截~

--- src/views/app/article/picArticle.vue
+++ src/views/app/article/picArticle.vue
@@ -4,45 +4,45 @@
     </div>
     <div class="main-style">
       <div class="center-style">
-        <el-form label-width="100px" style="padding-right: 20px;padding-top: 50px;padding-bottom: 30px;">
+        <el-form :model="articleForm" label-width="100px" style="padding-right: 20px;padding-top: 50px;padding-bottom: 30px;" ref="articleForm">
           <el-row>
             <el-col :span="8">
-              <el-form-item label="标题:">
-                <el-input placeholder="请在此处输入标题"></el-input>
+              <el-form-item label="标题:" prop="title">
+                <el-input placeholder="请在此处输入标题" v-model="articleForm.title"></el-input>
               </el-form-item>
             </el-col>
             <el-col :span="16">
-              <el-form-item label="作者:">
-                <el-input placeholder="请在此处输入作者"></el-input>
+              <el-form-item label="作者:" prop="author">
+                <el-input placeholder="请在此处输入作者" v-model="articleForm.author"></el-input>
               </el-form-item>
             </el-col>
           </el-row>
-          <el-form-item label="类别:">
-              <el-select placeholder="请选择" @click.native="getArticleType()">
-                <el-option
-                  v-for="item in articleType"
-                  :key="item.id"
-                  :label="item.name"
-                  :value="item.name">
-                </el-option>
-              </el-select>
+          <el-form-item label="类别:" prop="sortName">
+            <el-select placeholder="请选择" @click.native="getArticleType()" v-model="articleForm.sortName">
+              <el-option
+                v-for="item in articleType"
+                :key="item.id"
+                :label="item.name"
+                :value="item.name">
+              </el-option>
+            </el-select>
           </el-form-item>
           <el-row>
             <el-col :span="8">
               <el-form-item label="封面:">
                 <div class="avatar-uploader">
-                  <img-upload v-on:upload-pic="uploadPic"></img-upload>
+                  <img-upload v-on:upload-pic="uploadPic" :options="options"></img-upload>
                 </div>
               </el-form-item>
             </el-col>
             <el-col :span="16">
-              <el-form-item label="摘要:">
-                <el-input type="textarea" :rows="5" placeholder="请在此处输入摘要"></el-input>
+              <el-form-item label="摘要:" prop="introduction">
+                <el-input type="textarea" :rows="5" placeholder="请在此处输入摘要" v-model="articleForm.introduction"></el-input>
               </el-form-item>
             </el-col>
           </el-row>
-          <el-form-item label="正文内容:">
-            <rich-text height="800"></rich-text>
+          <el-form-item label="正文内容:" prop="content">
+            <rich-text height="800" v-model="articleForm.content"></rich-text>
           </el-form-item>
           <div class="footer">
             <el-form-item style="padding-top: 15px;padding-bottom: 10px">
@@ -54,6 +54,19 @@
         </el-form>
       </div>
     </div>
+    <el-dialog
+      title="提示"
+      :visible.sync="countdownDialog"
+      :show-close=false
+      :close-on-click-modal=true
+      :close-on-press-escape=false
+      width="30%"
+      center>
+      <span style="font-size:18px;">已成功添加文章！该窗口将于{{second}}秒后关闭！</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="window.close()" type="primary" :disabled="isDisabled">立即关闭</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -68,14 +81,49 @@ export default {
   },
   data: function () {
     return {
+      imageFile: {
+        name: '',
+        type: '',
+        file: ''
+      },
+      countdownDialog: false, // 控制弹出
+      second: 5, // 设置初始倒计时
+      isDisabled: false,
+      options: {
+        fixedNumber: [1, 1]
+      },
       editorOption: {
         placeholder: '输入文章内容'
       },
-      articleForm: {},
+      articleForm: {
+        sortName: '',
+        title: '',
+        author: '',
+        introduction: '',
+        picLink: '',
+        content: '',
+        creatTime: '',
+        updateTime: '',
+        titleStatus: ''
+      },
       articleType: []
     }
   },
   methods: {
+    async uploadPic (data) {
+      this.imageFile = data
+    },
+    getSecond () {
+      const that = this
+      const interval = window.setInterval(function () {
+        --that.second
+        if (that.second === 0) {
+          that.isDisabled = false
+          window.clearInterval(interval)
+          window.close()
+        }
+      }, 1000)
+    },
     getArticleType () {
       this.$api.requestApi.get('/basicCoding/findBasicCodingWithType/articleType')
         .then(res => {
@@ -95,7 +143,23 @@ export default {
           console.log(err.data)
         })
     },
-    submit () {
+    async addPicRecord () {
+      this.$api.requestApi.post('/article/addArticle', this.articleForm).then(res => {
+        this.countdownDialog = true
+        this.getSecond()
+      })
+    },
+    async submit () {
+      this.$refs.articleForm.validate((valid) => {
+        if (valid) {
+          console.log(this.imageFile)
+          this.$api.requestApi.post('/article/uploadPicture', this.imageFile)
+            .then(res => {
+              this.articleForm.picLink = res.data
+              this.addPicRecord()
+            }).catch()
+        }
+      })
     },
     preview () {
     }
